927f39fe28268a1d8fdaabc0dea9856adeabbe1b
unknown <chenxinliang@bit.edu.cn>
2018-09-10 10:44:41 +0800

    try add program_options

--- src/csar.cpp
+++ src/csar.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include <exception>
 #include <boost/program_options/cmdline.hpp>
+#include <boost/program_options/options_description.hpp>
 
 struct NotImplementedError: public std::exception
 {
 };
 
+
 int main(int argc, char const *argv[])
 {
+	using options_description = boost::program_options::options_description;
+	// options_description desc("Allowed options:");
 	std::cout << "Hello world!" << std::endl;
 	return 0;
 }
